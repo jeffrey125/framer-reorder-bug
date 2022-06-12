a7fbb7229a2adc14b4bc5ae62b92e291192c902c
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-12 14:47:32 +0800

    chore: added the comments for bug and solution

--- src/components/TestComp/Item.tsx
+++ src/components/TestComp/Item.tsx
@@ -18,9 +18,11 @@ export const Item = ({ item }: Props) => {
       id={item}
       className="relative flex justify-between items-center mb-5 bg-white p-5 rounded-lg list-none text-3xl w-full"
       style={{ boxShadow, y }}
+      // BUG when dragListener is false it highlights all the content you pass in when dragging to another place
       dragListener={false}
       dragControls={dragControls}
     >
+      {/*  put className="select-none" for temporary fix*/}
       <span>{item}</span>
       <SelectorIcon
         className="cursor-grab h-5 w-5 sm:h-7 sm:w-7  fill-darkFontColor"
