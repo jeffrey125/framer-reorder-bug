faa201992993ee82842b68a7646054e192f9fad2
jeffrey125 <jeffreyasilo8@gmail.com>
2022-06-12 14:25:34 +0800

    refactor: refactored code for list bug report on REORDER animation
    I followed the documentation and still it higlights the text when dragging an Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { useContext } from 'react';
-
-import TodoContext from './store/todo-context';
-import NewTodo from './components/NewTodo/NewTodo';
-import Todos from './components/Todos/Todos';
-import Card from './components/UI/Card';
-import MainBG from './components/UI/MainBG';
-import ThemeSwitcher from './components/UI/ThemeSwitcher';
+import Items from './components/TestComp/Items';
 
 function App() {
-  const todosCtx = useContext(TodoContext);
-
-  return (
-    // TODO Motion div opacity
-    <MainBG>
-      {/* TODO Motion div opacity */}
-      <Card>
-        <ThemeSwitcher />
-        <Todos items={todosCtx.todos} />
-        <NewTodo />
-      </Card>
-    </MainBG>
-  );
+  return <Items />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import TodoProvider from './store/TodoProvider';
-import ThemeProvider from './store/ThemeProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <TodoProvider>
-        <App />
-      </TodoProvider>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
